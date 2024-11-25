04cb394117b0f39cef3c0e7e75bccd27dd33bc11
KimJae5 <C24404366@mytudublin.ie>
2024-11-25 14:40:46 +0000

    sfx

---
+++ music-project/node_2d.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_red_boi_pressed() -> void:
+	$"red boi/AudioStreamPlayer".play()
+
+
+func _on_orange_boi_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_yellow_boi_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_green_boi_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_blue_boi_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_purple_boi_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_pink_boi_pressed() -> void:
+	pass # Replace with function body.
